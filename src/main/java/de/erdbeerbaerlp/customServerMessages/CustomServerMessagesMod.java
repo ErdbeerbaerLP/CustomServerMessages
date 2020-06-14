adcc629a7a587e12abf610d15f6c7f9d37e31bd8
ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>
2020-06-14 11:39:49 +0200

    Maybe fixed an mixin apply failed crash while starting

--- src/main/java/de/erdbeerbaerlp/customServerMessages/CustomServerMessagesMod.java
+++ src/main/java/de/erdbeerbaerlp/customServerMessages/CustomServerMessagesMod.java
@@ -23,7 +23,7 @@ import java.util.ArrayList;
 public class CustomServerMessagesMod {
     public static final String MODID = "servermsgs";
     public static final String NAME = "Custom Server Messages";
-    public static final String VERSION = "4.0.0";
+    public static final String VERSION = "4.0.1";
     /**
      * File for storing the last time the server needed to start up
      */
--- src/main/java/de/erdbeerbaerlp/customServerMessages/mixin/MixinStartupQuery.java
+++ src/main/java/de/erdbeerbaerlp/customServerMessages/mixin/MixinStartupQuery.java
@@ -7,12 +7,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 @Mixin(value = StartupQuery.class, remap = false)
 public class MixinStartupQuery {
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void query(String header, String text, String action, AtomicBoolean result, CallbackInfo ci) {
+    @Inject(method = "execute", at = @At("HEAD"))
+    private void query(CallbackInfo ci) {
         CustomServerMessagesMod.launchTimeValid = false;
     }
 }
